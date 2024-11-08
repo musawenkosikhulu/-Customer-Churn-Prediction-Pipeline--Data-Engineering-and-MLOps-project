d0ef7bf8fdd328135b208080c59e23b17110c8ad
Musawenkosi Khulu <musawenkosi@aims.ac.za>
2024-11-08 14:19:27 +0200

    Update deploy_model.py

--- deployment/deploy_model.py
+++ deployment/deploy_model.py
@@ -1,6 +1,6 @@
 def predict_churn(customer_data):
     # Load the model
-    model = joblib.load('/content/drive/MyDrive/churn_model.pkl')
+    model = joblib.load('models/')
 
     # Preprocess customer_data here (e.g., scaling, encoding)
     customer_data = scaler.transform(customer_data)
